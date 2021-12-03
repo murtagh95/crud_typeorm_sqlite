ab68eaa9279b111329a942d2fe457c801fb2b65c
Nicolas Catano <nec.catalano@gmail.com>
2021-12-03 11:42:31 -0300

    fix: se elimina una implementacion de interface en la entidad Appointments

--- src/entities/Appointments.ts
+++ src/entities/Appointments.ts
@@ -1,12 +1,11 @@
 import { Column, Entity, ManyToOne, JoinColumn, ManyToMany, JoinTable } from "typeorm";
 
 import { BaseEntity } from "./baseEntity";
-import { IEntities } from "./InterfaceEntities";
 import { Product } from "./Product";
 import { User } from "./User";
 
 @Entity("appointments")
-class Appointment extends BaseEntity implements IEntities {
+class Appointment extends BaseEntity {
 
   @ManyToOne(() => User, user => user.appointment)
   @JoinColumn({name: "user_fk_id"})
